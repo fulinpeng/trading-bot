f4e9302ea188280ca2c35dfdbdb7874a78efcdaf
fulinpeng <37953041+fulinpeng@users.noreply.github.com>
2024-11-07 18:19:44 +0800

    Delete test directory

--- test/strategies/test-demo.js
+++
@@ -1,7 +0,0 @@
-// 评估交易策略的函数（模拟返回策略的表现分数）
-function evaluateStrategy(params) {
-    const { param1, param2, param3 } = params;
-    // 示例: 用参数进行策略评估，并返回模拟的表现分数
-    return Math.random() * param1 + Math.random() * param2 - Math.random() * param3;
-}
-module.exports = { qualifiedSolutions };
